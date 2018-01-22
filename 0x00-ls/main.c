4d93299b47ec90809aead05ed12fb498f7b50b51
Daniel Alzugaray <dalzuga@gmail.com>
2018-01-22 15:07:54 -0800

    save

--- 0x00-ls/main.c
+++ 0x00-ls/main.c
@@ -8,17 +8,38 @@ int main(void)
 {
 	DIR *dir;
 	struct dirent *read;
+	int tmp;
 	
-	dir = opendir(".");
+	dir = NULL;
+	read = NULL;
+	tmp = 0;
 
-	read = readdir(dir);
-	while (read != NULL)
+	dir = opendir(".");
+	if (dir == NULL)
 	{
-		printf("%s\n", read->d_name);
-		read = readdir(dir);
+		perror("ls");
+		return (1);
 	}
 
-	closedir(dir);
+	read = readdir(dir);
+	while (1)
+	{
+		if (read == NULL)
+			break;
+
+		printf("%s\n", read->d_name);
+		read = readdir(dir);
+		if (read == NULL)
+			break;
+	}
+
+	tmp = closedir(dir);
+
+	if (tmp == -1)
+	{
+		perror("closedir");
+		return (1);
+	}
 
 	return (0);
 }
