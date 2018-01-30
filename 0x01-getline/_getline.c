e0af163999184fe6e5cfd29ce4a1957a9c8f39e0
Daniel Alzugaray <dalzuga@gmail.com>
2018-01-29 17:51:36 -0800

    save - start 0x01-getline

---
+++ 0x01-getline/_getline.c
@@ -0,0 +1,13 @@
+#include "_getline.h"
+
+/**
+ * _getline - reads a line from a file
+ *
+ * @fd: the file descriptor
+ *
+ * Return: The line read. NULL if there are no lines remaining; or, on error.
+ */
+char *_getline(const int fd)
+{
+
+}
---
+++ 0x01-getline/_getline.h
@@ -0,0 +1,11 @@
+#ifndef __GET_LINE_H_
+#define
+
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+char *_getline(const int fd);
+
+#endif
