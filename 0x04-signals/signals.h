82138cff3c524a605d83ebb61582a136ae7a078c
Daniel Alzugaray <dalzuga@gmail.com>
2018-03-14 15:02:34 -0700

    save

--- 0x04-signals/signals.h
+++ 0x04-signals/signals.h
@@ -6,7 +6,10 @@
 #include <stdio.h>
 #include <signal.h> 		/* signal(2) */
 
+/* handle_signal - handles a signal */
 int handle_signal(void);
+
+/* interrupt_handler - function called upon receiving a signal */
 void interrupt_handler(int signum);
 
 #endif
