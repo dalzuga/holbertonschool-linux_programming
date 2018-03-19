d7813dd280b38207dadaf729a075076d1660aa3c
Daniel Alzugaray <dalzuga@gmail.com>
2018-03-19 15:25:48 -0700

    save

--- 0x04-signals/1-current_handler_signal.c
+++ 0x04-signals/1-current_handler_signal.c
@@ -1,5 +1,8 @@
 #include "signals.h"
 
+/* include prototype */
+void print_hello(int);
+
 /**
  * current_handler_signal - gets the handler of the signal
  *
@@ -9,5 +12,10 @@
  */
 void (*current_handler_signal(void))(int signum)
 {
-	return (signal(SIGINT, SIG_DFL));
+	typedef void (*sighandler_t)(int);
+	sighandler_t curr = NULL;
+
+	curr = signal(SIGINT, print_hello);
+
+	return (curr);
 }
---
+++ 0x04-signals/1-set_print_hello.c
@@ -0,0 +1,21 @@
+#include "signals.h"
+
+/**
+ * print_hello - prints "Hello :)" and ignores the parameter
+ *
+ * @n: this parameter holds the number associated with the signal.
+ *
+ * Return: Nothing.
+ */
+void print_hello(int signum __attribute__((unused)))
+{
+	printf("Hello :)\n");
+}
+
+/**
+ * set_print_hello - this sets the signal to print_hello
+ */
+void set_print_hello(void)
+{
+	signal(SIGINT, print_hello);
+}
