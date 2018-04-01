34750a1d9f00c91ca34c13f78694b9b1f6d6f737
Daniel Alzugaray <dalzuga@gmail.com>
2018-04-01 09:14:06 -0700

    save - fixed

--- 0x05-nm_objdump/symbols64.c
+++ 0x05-nm_objdump/symbols64.c
@@ -9,19 +9,25 @@
  */
 int symbol_table64(FILE *f)
 {
-	ELF64_Ehdr *elf_header;
+	Elf64_Ehdr *elf_header;
 	int tmp = 0, tmp1 = 0;
 
-	tmp = fseek(f, 0, SEEK_SET);
+	elf_header = malloc(sizeof(Elf64_Ehdr));
+	if (elf_header == NULL)
+	{
+		perror("malloc");
+		exit(EXIT_FAILURE);
+	}
+	tmp = fseek(f, EI_DATA - 1, SEEK_SET);
 	if (tmp == -1)
 	{
 		perror("fseek");
 		exit(EXIT_FAILURE);
 	}
-
-	tmp1 = fread(elf_header->e_ident[EI_DATA], 1, 1, f);
+	tmp1 = fread(elf_header->e_ident + EI_DATA, 1, 1, f);
 
 	printf("tmp1: %d\n", tmp1);
+	printf("EI_DATA [hex]: %x\n", elf_header->e_ident[EI_DATA]);
 
 	return (1);
 }
