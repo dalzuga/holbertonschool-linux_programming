e98f7bf9f328db9bac6b72c8c55fc10f3bc74e3f
Daniel Alzugaray <dalzuga@gmail.com>
2018-04-02 14:40:02 -0700

    save - wrong way

--- 0x05-nm_objdump/symbols64.c
+++ 0x05-nm_objdump/symbols64.c
@@ -9,14 +9,14 @@
  */
 int p_symbol_table(FILE *f)
 {
-	Elf64_Ehdr *e_header = NULL;
+	Elf64_Ehdr *e_hdr = NULL;
 	Elf64_Sym *symtab = NULL;
 	Elf64_Shdr *s_hdr = NULL;
 	char *s_name = NULL;
-	int tmp = 0, tmp1 = 0;
+	int tmp = 0, tmp1 = 0, i = 0;
 
-	e_header = malloc(sizeof(Elf64_Ehdr));
-	if (e_header == NULL)
+	e_hdr = malloc(sizeof(Elf64_Ehdr));
+	if (e_hdr == NULL)
 	{
 		perror("malloc");
 		exit(EXIT_FAILURE);
@@ -26,22 +26,22 @@ int p_symbol_table(FILE *f)
 	if (tmp == -1)
 		exit(EXIT_FAILURE);
 
-	/* read into e_header */
-	tmp1 = fread(e_header, sizeof(Elf64_Ehdr), 1, f);
+	/* read into e_hdr */
+	tmp1 = fread(e_hdr, sizeof(Elf64_Ehdr), 1, f);
 	if (tmp1 != 1)
 		exit(EXIT_FAILURE);
 
-	printf("e_header->e_shoff: %lu\n", e_header->e_shoff);
-	printf("e_header->e_shnum: %d\n", e_header->e_shnum);
-	printf("e_header->e_shentsize: %d\n", e_header->e_shentsize);
+	printf("e_hdr->e_shoff: %lu\n", e_hdr->e_shoff);
+	printf("e_hdr->e_shnum: %d\n", e_hdr->e_shnum);
+	printf("e_hdr->e_shentsize: %d\n", e_hdr->e_shentsize);
 
-	if (e_header->e_shoff == 0) /* nothing to print */
+	if (e_hdr->e_shoff == 0) /* nothing to print */
 		return (1);	      /* success */
 
 	/*
          * not necessary
          * /\* seek into start of section header *\/
-	 * tmp = fseek(f, e_header->e_shoff, SEEK_SET);
+	 * tmp = fseek(f, e_hdr->e_shoff, SEEK_SET);
 	 * if (tmp == -1)
 	 * 	exit(EXIT_FAILURE);
          */
@@ -50,22 +50,34 @@ int p_symbol_table(FILE *f)
 	if (s_hdr == NULL)
 		exit(EXIT_FAILURE);
 
-	/* read into section hdr */
-	tmp1 = fread(s_hdr, sizeof(Elf64_Shdr), 1, f);
-	if (tmp1 != 1)
-		exit(EXIT_FAILURE);
+	for (i = 0; i < e_hdr->e_shnum; i++)
+	{
+		/* read section hdr */
+		tmp1 = fread(s_hdr, e_hdr->e_shentsize, 1, f);
+		if (tmp1 != 1)
+			exit(EXIT_FAILURE);
 
-	/* seek into name of a section */
-	tmp = fseek(f, s_hdr->sh_name, SEEK_SET);
-	if (tmp == -1)
-		exit(EXIT_FAILURE);
-	s_name = malloc(sizeof(char) * 20);
-	fread(s_name, sizeof(char) * 20, 1, f);
+		/*
+                 * not the right way
+                 * /\* seek into name of a section *\/
+		 * tmp = fseek(f, s_hdr->sh_name, SEEK_SET);
+		 * if (tmp == -1)
+		 * 	exit(EXIT_FAILURE);
+                 */
 
-	printf("s_name: %s\n", s_name);
+		if (s_hdr->sh_type == SHT_STRTAB)
+		{}
+		printf("sh_type: %ull\n", s_hdr->sh_type);
+	}
 
-	if (0)
+	printf("MACRO: %ull\n", SHT_STRTAB);
+
+	if (0)			/* avoid compiler unused warning */
+	{
 		printf("%p", (void *) symtab);
+		printf("s_name: %s\n", s_name);
+	}
+
 
 	return (1);
 }
