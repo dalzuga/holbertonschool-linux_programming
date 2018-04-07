4ffddec79f00c1889ca678554f7859ca9406f207
Daniel Alzugaray <dalzuga@gmail.com>
2018-04-07 13:47:53 -0700

    save - section header working

--- 0x05-nm_objdump/symbols64.c
+++ 0x05-nm_objdump/symbols64.c
@@ -38,29 +38,35 @@ int p_symbol_table(FILE *f)
 	if (e_hdr->e_shoff == 0) /* nothing to print */
 		return (1);	      /* success */
 
-        /* seek into start of section header */
-	tmp = fseek(f, e_hdr->e_shoff, SEEK_SET);
-	if (tmp == -1)
-		exit(EXIT_FAILURE);
+	printf("e_shstrndx: %d\n", e_hdr->e_shstrndx);
+	printf("e_shstrndx SHN_UNDEF: %d\n", SHN_UNDEF);
+	printf("sizeof(Elf64_Shdr): %lu\n", sizeof(Elf64_Shdr));
+	printf("sizeof(uint32_t): %lu\n", sizeof(uint32_t));
+	printf("sizeof(uint64_t): %lu\n", sizeof(uint64_t));
 
 	s_hdr = malloc(sizeof(Elf64_Shdr));
 	if (s_hdr == NULL)
 		exit(EXIT_FAILURE);
 
-	printf("e_shstrndx: %d\n", e_hdr->e_shstrndx);
-	printf("e_shstrndx SHN_UNDEF: %d\n", SHN_UNDEF);
+        /* seek into start of section header */
+	tmp = fseek(f, e_hdr->e_shoff, SEEK_SET);
+	if (tmp == -1)
+		exit(EXIT_FAILURE);
 
+	/* read the first section header */
 	tmp1 = fread(s_hdr, e_hdr->e_shentsize, 1, f);
 	if (tmp1 != 1)
 		exit(EXIT_FAILURE);
 
-	printf("%s\n", (char *) s_hdr + (s_hdr->sh_name));
+	while (s_hdr->sh_type == SHT_NULL)
+	{
+		tmp1 = fread(s_hdr, e_hdr->e_shentsize, 1, f);
+		if (tmp1 != 1)
+			exit(EXIT_FAILURE);
+	}
+	printf("%lu\n", (long unsigned) s_hdr->sh_type);
 
-	tmp = fseek(f, e_hdr->e_shoff + e_hdr->e_shstrndx, SEEK_SET);
-	if (tmp == -1)
-		exit(EXIT_FAILURE);
-
-	printf("MACRO: %ull\n", SHT_STRTAB);
+	/* printf("MACRO: %ull\n", SHT_STRTAB); */
 
 	/*
          * for (i = 0; i < e_hdr->e_shnum; i++)
